9738490f9e447957ebcee13e2a8feea1d8ee8298
yongku.lee <yongku.lee@firmainnovation.com>
2025-07-08 14:45:45 +0900

    chore: fix minor warnings

--- packages/documents/vite.config.ts
+++ packages/documents/vite.config.ts
@@ -1,10 +1,11 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
-	plugins: [react(), libInjectCss()],
+	plugins: [react(), libInjectCss(), dts()],
 	css: {
 		postcss: "./postcss.config.js",
 	},
